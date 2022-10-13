a688c7609fc02a7c763941a77b9db1b332aa180f
Noa Senesi <noa.senesi@gmail.com>
2022-10-13 15:28:54 +0200

    Language button now works

--- index.php
+++ index.php
@@ -4,6 +4,18 @@ include "lang.php";
 $d1 = new DateTime();
 $d2 = new DateTime("2002-06-27");
 $age = $d2->diff($d1)->y;
+
+if (isset($_POST)) {
+	$lang = "";
+	if (isset($_POST["fr"])) $lang = "fr_fr";
+	if (isset($_POST["uk"])) $lang = "en_uk";
+	if (isset($_POST["de"])) $lang = "de_de";
+
+	if ($lang) {
+		setcookie("lang_id", $lang, strtotime("+365 days"), "/");
+		header("Location: " . str_replace("index.php", "", $_SERVER["PHP_SELF"]));
+	}
+}
 ?>
 <!DOCTYPE html>
 <html>
