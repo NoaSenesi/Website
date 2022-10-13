a3d4ca8864879384a4f1f95a3b8260181dadb8db
Noa Senesi <noa.senesi@gmail.com>
2022-10-13 14:57:11 +0200

    Added automatic language

--- lang.php
+++ lang.php
@@ -1,3 +1,10 @@
 <?php
-include "lang/en_uk.php";
+$lang_id = explode("_", str_replace(array("-"), array("_"), explode(",", strtolower($_SERVER["HTTP_ACCEPT_LANGUAGE"]))[0]));
+
+if ($lang_id[0] == "fr") $lang_id = "fr_fr";
+else if ($lang_id[0] == "en"/* && ($lang[1] == "uk" || $lang_id[1] == "gb")*/) $lang_id = "en_uk";
+else if ($lang_id[0] == "de") $lang_id = "de_de";
+else $lang_id = "en_uk";
+
+include "lang/" . $lang_id . ".php";
 ?>
