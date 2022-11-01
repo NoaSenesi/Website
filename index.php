2dc89c39e73b33aeb8d8b622c1653305989c6bc1
Noa Senesi <noa.senesi@gmail.com>
2022-11-01 21:52:52 +0100

    Started redesign, created header

--- index.php
+++ index.php
@@ -25,131 +25,15 @@ if (isset($_POST)) {
 		<link rel="stylesheet" href="style.css">
 		<link rel="stylesheet" href="responsive.css">
 		<link rel="icon" href="img/logo.png">
+		<script src="script.js" defer></script>
 		<title>Noa Senesi</title>
 	</head>
 	<body>
 		<header>
-			<div class="introduction">
-				<span><?= $_LANG["introduction"] ?></span>
-				<span class="typing">&nbsp;</span>
-			</div>
+			<div class="introduction"><span>Hello, I am</span><span class="name">Noa Senesi</span></div>
 		</header>
-		<div class="lang">
-			<img src="img/flags/<?= $_FLAG ?>.png" draggable="false">
-			<div class="other">
-				<form method="POST">
-					<?php if ($_FLAG != "fr") { ?>
-					<button name="fr">
-						<img src="img/flags/fr.png" draggable="false">
-					</button>
-					<?php } if ($_FLAG != "uk") { ?>
-					<button name="uk">
-						<img src="img/flags/uk.png" draggable="false">
-					</button>
-					<?php } if ($_FLAG != "de") { ?>
-					<button name="de">
-						<img src="img/flags/de.png" draggable="false">
-					</button>
-					<?php } ?>
-				</form>
-			</div>
-		</div>
-		<div class="space space-header"></div>
-		<article class="about-me">
-			<div class="image">
-				<img src="img/logo.png" draggable="false">
-			</div>
-			<div class="container">
-				<div class="title"><?= $_LANG["about_me"] ?></div>
-				<div class="content"><?= str_replace("{age}", $age, $_LANG["about_me_text"]) ?></div>
-			</div>
-		</article>
-		<div class="space space-about-me"></div>
-		<article class="block information">
-			<div class="title"><?= $_LANG["information"] ?></div>
-			<div class="container">
-				<?php
-				foreach ($_LANG["information_elements"] as $info) {
-				?>
-				<div><?= $info["name"] ?>: <?= $info["value"] ?></div>
-				<?php
-				}
-				?>
-			</div>
-		</article>
-		<hr>
-		<article class="block background">
-			<div class="title"><?= $_LANG["background"] ?></div>
-			<div class="container">
-				<?php
-				foreach ($_LANG["background_elements"] as $bg) {
-				?>
-				<div class="part<?= isset($bg["content"]) ? " content" : "" ?>">
-					<div><?= $bg["title"] ?></div>
-					<?php
-					foreach ($bg["description"] as $d) {
-					?>
-					<div><?= $d ?></div>
-					<?php
-					}
-
-					if (isset($bg["content"])) {
-					?>
-					<div class="content">
-						<?= $bg["content"] ?>
-					</div>
-					<?php
-					}
-					?>
-				</div>
-				<?php
-				}
-				?>
-			</div>
-		</article>
-		<hr>
-		<article class="block skills">
-			<div class="title"><?= $_LANG["skills"] ?></div>
-			<div class="container">
-				<?php
-				foreach ($_LANG["skills_elements"] as $skill) {
-				?>
-				<div class="subtitle"><?= $skill["subtitle"] ?></div>
-				<?php
-				foreach ($skill["elements"] as $el) {
-				?>
-				<div class="part">
-					<div class="skill"><?= $el["name"] ?>:</div>
-					<div class="bar" style="--width: <?= $el["value"] ?>%"></div>
-				</div>
-				<?php
-				}}
-				?>
-			</div>
-		</article>
-		<hr>
-		<article class="block links">
-			<div class="title"><?= $_LANG["links"] ?></div>
-			<div class="container">
-				<div>
-					<?php
-					$br = false;
-					foreach ($_LANG["links_elements"] as $link) {
-						if ($br) {
-					?>
-					<br>
-					<?php
-						}
-					?>
-					<a href="<?= $link["value"] ?>" target="_blank"><?= $link["name"] ?></a>
-					<?php
-					$br = true;
-					}
-					?>
-				</div>
-			</div>
-		</article>
-		<div class="space"></div>
-		<script src="script.js"></script>
+		<?php for ($i = 0; $i < 100; $i++) { ?>
+		<h1>Test</h1>
+		<?php } ?>
 	</body>
 </html>
